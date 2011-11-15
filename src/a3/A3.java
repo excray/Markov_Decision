99de398f038d4abc9bb8daf44d10b7a1b6b080a8
gvivekcbe <gvivekcbe@yahoo.com>
2011-11-15 00:28:26 -0500

    second

--- src/a3/A3.java
+++ src/a3/A3.java
@@ -8,16 +8,26 @@ package a3;
  *
  * @author Vivek
  */
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.Collections;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.LinkedHashMap;
-
+import java.util.Stack;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.omg.CORBA.Environment;
 
 public class A3 {
+
+    private static Table<Double> table;
 
     /**
      * @param args the command line arguments
@@ -29,20 +39,9 @@ public class A3 {
         runProblem1();
     }
 
-    private static void runProblem1() {
-
-        double reward_min = 1;
-        double reward_max = 10;
-        double rmid = trunc((reward_max - reward_min) / 2);
-
-        ArrayList<Double> rewardVals = new ArrayList<Double>();
-        BinaryTreeVals(reward_min, rmid, reward_max, rewardVals);
-
-        int xdim = 4;
-        int ydim = 3;
-
+    private static Map<Cell<Double>, CellAction> GetOptimalAction(int xdim, int ydim, double r) {
         //Create Table
-        Table<Double> table = createTable(Double.class, xdim, ydim, -0.04);
+        table = createTable(Double.class, xdim, ydim, r);
 
         table.removeCell(2, 2);
         table.getCellAt(4, 3).setContent(1.0);
@@ -56,29 +55,223 @@ public class A3 {
 
         Map<Cell<Double>, Double> U = vi.valueIteration(mdp, 0.0001, optimalAction);
 
+        return optimalAction;
+    }
 
-        //Iterate through each cell and get the optimal policy for that cell(state)
-        for (int j = ydim; j > 0; j--) {
-            for (int i = 1; i <= xdim; i++) {
-                Cell<Double> c = table.getCellAt(i, j);
+    private static boolean compareMaps(Map<Cell<Double>, CellAction> a, Map<Cell<Double>, CellAction> b) {
+        List<CellAction> values1 = new ArrayList<CellAction>(a.values());
+        List<CellAction> values2 = new ArrayList<CellAction>(b.values());
+        //boolean equals = true;
+        for (int i = 0; i < values1.size(); i++) {
+            CellAction a1 = values1.get(i);
+            CellAction b1 = values2.get(i);
 
-                if (c != null) {
-                    CellAction ca = optimalAction.get(c);
+            if (a1.equals(b1) == false) {
 
-                    if (ca != null) {
-                        System.out.print(ca.toString() + "\t");
-                    } else {
-                        System.out.print(c.getContent().toString() + "\t");
-                    }
-                } else {
-                    System.out.print("X\t");
-                }
+                return false;
             }
-
-            System.out.println();
         }
 
-        System.out.println();
+        return true;
+    }
+
+    private static void runProblem1() {
+
+//        double reward_min = 1;
+//        double reward_max = 10;
+//        double rmid = trunc((reward_max - reward_min) / 2);
+
+        //BinaryTreeVals(reward_min, rmid, reward_max);
+
+        int xdim = 4;
+        int ydim = 3;
+
+        double l = 0.0, r = -2.0, mid = 0.0;
+
+        ArrayList<Double> allThresholds = new ArrayList();
+        ArrayList<Double> finalThresholds = new ArrayList();
+
+        ArrayList<Map<Cell<Double>, CellAction>> allActions = new ArrayList<Map<Cell<Double>, CellAction>>();
+        Stack<Double> stackHolder = new Stack<Double>();
+        stackHolder.push(r);
+        stackHolder.push(l);
+        Map<Double, Map<Cell<Double>, CellAction>> actionMap = new LinkedHashMap<Double, Map<Cell<Double>, CellAction>>();
+
+        while (stackHolder.empty() == false) {
+
+
+            l = stackHolder.pop();
+            r = stackHolder.pop();
+
+            boolean leftFound = false, rightFound = false;
+
+            if (Math.abs(r - l) > 0.0001) {
+
+                mid = (l + r) / 2.0;
+
+                Map<Cell<Double>, CellAction> optimalActionl = GetOptimalAction(xdim, ydim, l);
+                Map<Cell<Double>, CellAction> optimalActionm = GetOptimalAction(xdim, ydim, mid);
+                Map<Cell<Double>, CellAction> optimalActionr = GetOptimalAction(xdim, ydim, r);
+
+                if (compareMaps(optimalActionr, optimalActionm) == false) {
+                    stackHolder.push(r);
+                    stackHolder.push(mid);
+                    leftFound = true;
+                }
+
+                if (compareMaps(optimalActionl, optimalActionm) == false) {
+                    stackHolder.push(mid);
+                    stackHolder.push(l);
+                    // allThresholds.add(mid);
+                    rightFound = true;
+                }
+
+                if (leftFound || rightFound) {
+                    allThresholds.add(mid);
+                    actionMap.put(mid, optimalActionm);
+                }
+            }
+        }
+
+
+        //   allThresholds.add(0.0);
+        // actionMap.put(0.0, GetOptimalAction(xdim, ydim, 0.0));
+        Collections.sort(allThresholds);
+
+        int t = 0;
+        for (int i = allThresholds.size() - 1; i > 0; i--) {
+            Map<Cell<Double>, CellAction> c1 = actionMap.get(allThresholds.get(i));
+            Map<Cell<Double>, CellAction> c2 = actionMap.get(allThresholds.get(i - 1));
+            if (compareMaps(c1, c2) == false) {
+                t = i;
+                finalThresholds.add(allThresholds.get(i));
+            }
+        }
+
+        finalThresholds.add(allThresholds.get(t - 1));
+
+
+        //  Collections.sort(finalThresholds);
+
+        try {
+            // Create file 
+            FileWriter fstream = new FileWriter("P1-output.txt");
+            BufferedWriter out = new BufferedWriter(fstream);
+
+            //first threshold
+            // out.write("R(s)=0.0\n");
+            //out.write(System.getProperty("line.separator"));
+
+            // printOptimal(xdim, ydim, actionMap.get(0.0), null, out);
+            Map<Cell<Double>, CellAction> t1 = actionMap.get(finalThresholds.get(1));
+            Map<Cell<Double>, CellAction> t2;
+//            
+//            out.write(finalThresholds.get(0).toString() + " > R(s) > " + finalThresholds.get(1).toString() + "\n");
+//            out.write(System.getProperty("line.separator"));
+//
+//           // t2 = actionMap.get(finalThresholds.get(i + 1));
+//            printOptimal(xdim, ydim, t1, null, out);
+//            printOptimal(xdim, ydim, t1, null, out);
+
+
+            String lb = "0.0";
+
+            for (int i = 0; i < finalThresholds.size(); i++) {
+                String ub = finalThresholds.get(i).toString();
+                ub = ub.substring(0,8);
+                if (i != finalThresholds.size() - 1) {
+                    out.write(lb + " > R(s) > " + ub + "\n");
+                } else {
+                    out.write("INF > R(s) > " + lb + "\n");
+                }
+                out.write(System.getProperty("line.separator"));
+
+
+                t2 = actionMap.get(finalThresholds.get(i));
+                if (i == 0) {
+                    printOptimal(xdim, ydim, t2, null, out);
+                } else {
+                    printOptimal(xdim, ydim, t2, t1, out);
+                }
+
+                t1 = t2;
+                lb = ub;
+            }
+
+//            out.write("R(s) > " + finalThresholds.get(finalThresholds.size()-1).toString() + "\n");           //Close the output stream
+//            out.write(System.getProperty("line.separator"));
+//
+//            t2 = actionMap.get(finalThresholds.get(finalThresholds.size()-2));
+//            printOptimal(xdim, ydim, t1, t2, out);
+            out.close();
+
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+        // System.out.println(finalThresholds.size());
+    }
+
+    private static CellAction getCellAction(int i, int j, Map<Cell<Double>, CellAction> mp) {
+        Iterator it = mp.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pairs = (Map.Entry) it.next();
+            Cell<Double> c = (Cell<Double>) pairs.getKey();
+            if (c.getX() == i && c.getY() == j) {
+                return (CellAction) pairs.getValue();
+            }
+            //System.out.println(pairs.getKey() + " = " + pairs.getValue());
+            //it.remove(); // avoids a ConcurrentModificationException
+        }
+        return null;
+    }
+
+    private static void printOptimal(int xdim, int ydim, Map<Cell<Double>, CellAction> optimalAction, Map<Cell<Double>, CellAction> optimalAction1, BufferedWriter out) {
+        try {
+            //  List<CellAction> values1 = new ArrayList<CellAction>(optimalAction.values());
+            //   List<CellAction> values2 = new ArrayList<CellAction>(optimalAction1.values());
+            //Iterate through each cell and get the optimal policy for that cell(state)
+            for (int j = ydim; j > 0; j--) {
+                for (int i = 1; i <= xdim; i++) {
+                    Cell<Double> c = table.getCellAt(i, j);
+
+                    String toWrite = "";
+                    if (c != null) {
+                        CellAction ca = getCellAction(i, j, optimalAction);
+
+                        if (ca != null) {
+                            toWrite = ca.toString();
+                        }
+                        if (optimalAction1 != null) {
+                            CellAction ca1 = getCellAction(i, j, optimalAction1);
+                            if (ca != null && ca1 != null) {
+                                if (ca1.equals(ca) == false) {
+                                    toWrite = ca.toString() + "*";
+                                }
+                            }
+                        }
+
+                        if (ca != null) {
+
+                            out.write(toWrite + "\t");
+
+                        } else {
+                            out.write(c.getContent().toString() + "\t");
+                        }
+                    } else {
+                        out.write("X\t");
+                    }
+                }
+
+                out.write(System.getProperty("line.separator"));
+
+            }
+            out.write(System.getProperty("line.separator"));
+            out.write(System.getProperty("line.separator"));
+
+        } catch (IOException ex) {
+            Logger.getLogger(A3.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
     private static double trunc(double d) {
@@ -86,32 +279,8 @@ public class A3 {
         double dl = (double) (d * 1);
         dl = Math.ceil(dl);
         double n = (double) (dl / 1);//0.0);
-        long nl = (long)(n);
-        return (double)(nl/1.0);
-    }
-
-    private static void BinaryTreeVals(Double l, Double mid, Double r, ArrayList<Double> v) {
-
-        l = trunc(l);
-        r = trunc(r);
-        mid = trunc(mid);
-
-        if (mid.compareTo(l) <= 0 || mid.compareTo(r) >= 0) {
-            return;
-        }
-
-
-        r = mid;
-        mid = (r - l) / 2;
-        BinaryTreeVals(l, mid, r, v);
-        v.add(r);
-
-        l = mid;
-        mid = l + (r - l) / 2;
-        BinaryTreeVals(l, mid, r, v);
-
-        v.add(l);
-
+        long nl = (long) (n);
+        return (double) (nl / 1.0);
     }
 
     private static <T> Table< T> createTable(Class<T> c, int xdim, int ydim, Object d) {
